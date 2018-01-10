739739f71aa719bd335b87129cd8ef5430b7f7e5
shishenzhezai <1316147236@qq.com>
2018-01-10 16:13:01 +0800

    test pass

--- MyBlog/views.py
+++ MyBlog/views.py
@@ -1,3 +1,22 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import ForumQuestions
+
+
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def querytion_list(request):
+    question_list = ForumQuestions.objects
+    template = loader.get_template('MyBlog/list.html')
+    context = {
+        'question_list': question_list
+    }
+    return HttpResponse(template.render(context), request)
