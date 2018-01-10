6b9da01d6b51b8118f1d0ec45a6ee3e339a65d05
huanmengfeiyu <lds1316147236@gmail.com>
2018-01-10 20:19:32 +0800

    no go 2

--- MyBlog/views.py
+++ MyBlog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from .models import Sys_User, Forum_Questions, Tags
+
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = "MyBlog/index.html"
+    context_object_name = ""
+
+    def get_queryset(self):
+        return HttpResponseRedirect("Hello")
